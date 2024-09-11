733c4099782534c8f1d196a0fba263a21512335c
Bayu Kurniawan <bayu05061992@gmail.com>
2024-09-11 22:29:39 +0700

    section portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        urbanist: ['urbanist']
+      },
+      colors: {
+        'primary': '#FD853A'
+      }
+    },
   },
   plugins: [],
 }
